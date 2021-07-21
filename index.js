4c9660e01a4a29a84cca8e229b03442d4cacefbb
Nate Nelson <nathan.nelson99@gmail.com>
2021-07-21 15:13:33 -0600

    intermediate portion

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ function addMovie(event) {
 
 function deleteMovie(event) {
     event.target.parentNode.remove()
-    message.textContent = "Movie deleted!"
+    message.textContent = `${event.target.parentNode.firstChild.textContent} deleted!`
+    revealMessage();
 }
 
 document.querySelector('form').addEventListener('submit', addMovie);
@@ -30,9 +31,14 @@ document.querySelector('form').addEventListener('submit', addMovie);
 function crossOffMovie(event) {
     event.target.classList.toggle('checked')
     if(event.target.classList.contains('checked')) {
-         message.textContent = "Movie watched!"
+         message.textContent = `${event.target.textContent} watched!`
     } else {
-        message.textContent = "Movie added back!"
+        message.textContent = `${event.target.textContent} added back!`
     }
+    revealMessage();
 }
 
+function revealMessage() {
+    message.classList.remove('hide')
+    setTimeout(() => message.classList.toggle('hide'), 1000)
+}
